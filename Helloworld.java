e68e0f0232f53bbe37e65e774fde0ab4cc9a9eb7
penguikai2009 <80452926@qq.com>
2022-09-11 17:40:58 +0800

    delete Helloworld.java

--- Helloworld.java
+++
@@ -1,9 +0,0 @@
-public class Helloworld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		int a = 10;
-		int b = 20;
-		int c = a + b;
-		System.out.println(c);
-	}
-}
