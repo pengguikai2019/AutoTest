e7938f772faa366bea65e607b597466826ea48e4
penguikai2009 <80452926@qq.com>
2022-09-11 17:18:01 +0800

    modification in idea

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,9 +1,11 @@
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		int a = 10;
-		int b = 20;
+		int a = 100;
+		int b = 200;
 		int c = a + b;
 		System.out.println(c);
+
+
 	}
 }
